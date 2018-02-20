fdb0e32000d107aab37db9484e5d448d48bf9ba6
Sergey Cherkesov <go.for.broke1006@gmail.com>
2018-02-21 02:27:03 +0300

    Debug create IAM EC2 role script

---
+++ create-iam-profile-ec2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo 'Enter project name (HotelHeroApp): '
+read projectName
+
+aws iam create-role \
+    --role-name ${projectName}-EC2-Instance-Profile \
+    --assume-role-policy-document file://EC2-Trust.json
+
+aws iam put-role-policy \
+    --role-name ${projectName}-EC2-Instance-Profile \
+    --policy-name ${projectName}-EC2-Permissions \
+    --policy-document file://EC2-Permissions.json
+
+aws iam create-instance-profile \
+    --instance-profile-name ${projectName}-EC2-Instance-Profile
+
+aws iam add-role-to-instance-profile \
+    --instance-profile-name ${projectName}-EC2-Instance-Profile \
+    --role-name ${projectName}-EC2-Instance-Profile
+
