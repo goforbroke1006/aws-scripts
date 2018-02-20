79500662b78253749aafce3780dfcac322bda5c8
Sergey Cherkesov <go.for.broke1006@gmail.com>
2018-02-20 15:16:18 +0300

    Initial commit

--- ec2-iam-role.sh
+++ ec2-iam-role.sh
@@ -1,4 +1,4 @@
-
+echo 'Enter project name'
 
 aws iam create-role --role-name CodeDeployDemo-EC2-Instance-Profile --assume-rolepolicy-document
 file://CodeDeployDemo-EC2-Trust.json
@@ -6,3 +6,8 @@ file://CodeDeployDemo-EC2-Trust.json
 aws iam put-role-policy --role-name CodeDeployDemo-EC2-Instance-Profile --policyname
 CodeDeployDemo-EC2-Permissions --policy-document file://CodeDeployDemo-EC2-
 Permissions.json
+
+aws iam create-instance-profile --instance-profile-name CodeDeployDemo-EC2-InstanceProfile
+aws iam add-role-to-instance-profile --instance-profile-name CodeDeployDemo-EC2-
+Instance-Profile --role-name CodeDeployDemo-EC2-Instance-Profile
+
